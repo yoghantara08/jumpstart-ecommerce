8e9df7f1f182b7ceaec2c23b6b76c90399fd8125
Yoghantara <gyoghantara@gmail.com>
2023-03-20 16:08:40 +0800

    order detail

---
+++ client/components/website/user/order-detail.tsx
@@ -0,0 +1,84 @@
+import { IMAGE_URL } from "@/lib/config";
+import { IOrder } from "@/types/products-type";
+import formatDate from "@/utils/format-date";
+import { Dialog } from "@headlessui/react";
+import React from "react";
+import { BiX } from "react-icons/bi";
+
+interface Props {
+  order: IOrder;
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const OrderDetails: React.FC<Props> = ({ isOpen, order, setIsOpen }) => {
+  const closeHandler = () => {
+    setIsOpen(false);
+  };
+
+  const date = formatDate(order.createdAt);
+
+  return (
+    <Dialog
+      open={isOpen}
+      onClose={() => setIsOpen(false)}
+      as="div"
+      className="relative z-50"
+    >
+      <Dialog.Overlay className="fixed inset-5 bg-black opacity-30 top-0 left-0 w-full h-full" />
+      <div className="fixed inset-0 flex items-center justify-center p-4">
+        <Dialog.Panel className="w-full max-w-md rounded-lg bg-white shadow">
+          <Dialog.Title className="border-b px-5 py-3 flex justify-between items-center gap-3 text-slate-700">
+            <span className="font-montserrat font-medium">Order Details</span>
+            <BiX className="text-2xl cursor-pointer" onClick={closeHandler} />
+          </Dialog.Title>
+          <div className="px-5 py-3">
+            <p>
+              Order Id: <span className="text-yellow-600">#{order._id}</span>
+            </p>
+            <p>Date: {date}</p>
+            <div className="grid grid-cols-4 border-b mt-1 py-2">
+              <h4>Product</h4>
+              <h4 className="col-span-2">Product Name</h4>
+              <h4>Price</h4>
+            </div>
+            {order.items.map((item) => (
+              <div
+                className="grid grid-cols-4 border-b py-2"
+                key={item.product._id}
+              >
+                <picture className="w-14 h-12 flex overflow-hidden">
+                  <img
+                    src={`${IMAGE_URL}/${item.product.image}`}
+                    alt={item.product.name}
+                    className="w-full h-full"
+                  />
+                </picture>
+                <div className="col-span-2">
+                  <p>{item.product.name}</p>
+                  <p className="text-sm text-slate-500">Qty: {item.quantity}</p>
+                </div>
+                <p>${item.price}</p>
+              </div>
+            ))}
+            <div className="grid grid-cols-4 border-b py-3">
+              <p className="col-span-3">Total: </p>
+              <p>${order.totalPrice}</p>
+            </div>
+          </div>
+
+          <div className="flex justify-end pb-4 pt-1 px-5">
+            <button
+              className="rounded bg-slate-500 text-white px-5 py-2 font-medium"
+              onClick={closeHandler}
+            >
+              Close
+            </button>
+          </div>
+        </Dialog.Panel>
+      </div>
+    </Dialog>
+  );
+};
+
+export default OrderDetails;
--- client/components/website/user/orders-history.tsx
+++ client/components/website/user/orders-history.tsx
@@ -4,6 +4,7 @@ import formatDate from "@/utils/format-date";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState, useEffect } from "react";
+import OrderDetails from "./order-detail";
 
 interface Props {
   order: IOrder;
@@ -11,6 +12,7 @@ interface Props {
 
 const OrdersHistory: React.FC<Props> = ({ order }) => {
   const [statusColor, setStatusColor] = useState("");
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   const date = formatDate(order.createdAt);
 
   useEffect(() => {
@@ -29,52 +31,55 @@ const OrdersHistory: React.FC<Props> = ({ order }) => {
   }, [order.status]);
 
   return (
-    <div className="py-4 px-5 rounded border border-gray-400 mb-4">
-      <div className="flex flex-col md:flex-row md:gap-3 font-medium font-montserrat">
-        <span>Order</span>
-        <Link
-          href={`/orders/${order._id}`}
-          className="text-yellow-600 hover:underline"
-        >
-          #{order._id}
-        </Link>
-        <span>{date}</span>
-        <span className={statusColor}>{order.status}</span>
-      </div>
-      <div className="w-full flex flex-col mt-3 gap-3">
-        {order.items.map((item) => (
-          <div key={item.product._id} className="flex gap-3">
-            <div className="w-16 h-14">
-              <Image
-                src={`${IMAGE_URL}/${item.product.image}`}
-                alt={item.product.name}
-                width={150}
-                height={150}
-                className="w-full h-full rounded"
-              />
-            </div>
-            <div className="md:text-lg">
-              <p>{item.product.name}</p>
-              <p className="font-medium">
-                {item.quantity} x ${item.price}
-              </p>
-            </div>
-          </div>
-        ))}
-      </div>
-      <div className="flex gap-3 items-center justify-between mt-3">
-        <div className="md:text-lg">
-          <p>Total Amount</p>
-          <p className="font-medium">${order.totalPrice}</p>
+    <>
+      <OrderDetails isOpen={isOpen} order={order} setIsOpen={setIsOpen} />
+      <div className="py-4 px-5 rounded border border-gray-400 mb-4">
+        <div className="flex flex-col md:flex-row md:gap-3 font-medium font-montserrat">
+          <span>Order</span>
+          <Link
+            href={`/user/orders/${order._id}`}
+            className="text-yellow-600 hover:underline"
+          >
+            #{order._id}
+          </Link>
+          <span>{date}</span>
+          <span className={statusColor}>{order.status}</span>
         </div>
-        <Link
-          href={`/orders/${order._id}`}
-          className="block button-primary py-2"
-        >
-          Order Details
-        </Link>
+        <div className="w-full flex flex-col mt-3 gap-3">
+          {order.items.map((item) => (
+            <div key={item.product._id} className="flex gap-3">
+              <div className="w-16 h-14">
+                <Image
+                  src={`${IMAGE_URL}/${item.product.image}`}
+                  alt={item.product.name}
+                  width={150}
+                  height={150}
+                  className="w-full h-full rounded"
+                />
+              </div>
+              <div className="md:text-lg">
+                <p>{item.product.name}</p>
+                <p className="font-medium">
+                  {item.quantity} x ${item.price}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="flex gap-3 items-center justify-between mt-3">
+          <div className="md:text-lg">
+            <p>Total Amount</p>
+            <p className="font-medium">${order.totalPrice}</p>
+          </div>
+          <button
+            className="button-primary py-2"
+            onClick={() => setIsOpen(true)}
+          >
+            Order Details
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
